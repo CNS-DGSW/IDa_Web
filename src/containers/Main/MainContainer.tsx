3ab44b4466b781623c92fa57138feb7360d13b1a
Jominhui <rihito2021@naver.com>
2020-10-23 14:35:38 +0900

    [ADD] MainPage

--- src/containers/Main/MainContainer.tsx
+++ src/containers/Main/MainContainer.tsx
@@ -1,10 +1,11 @@
+import Main from "components/Main";
 import { inject, observer } from "mobx-react";
 import React from "react";
 
 const MainContainer = () => {
   return (
     <>
-      <div></div>
+      <Main />
     </>
   );
 };
