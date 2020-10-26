52ff3a1a6f1368a8004819b46b64916671d72402
정성훈 <52942411+jsh0128@users.noreply.github.com>
2020-10-26 20:52:00 +0900

    [ADD] login design add
    로그인 디자인 추가

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -6,7 +6,30 @@ interface LoginProps {}
 const Login = ({}: LoginProps) => {
   return (
     <>
-      <div></div>
+      <div className="Login">
+        <div className="Login-text">
+          <span className="Login-text-Welcome">Welcome</span>
+          <span className="Login-text-content">아아디 비번 똑띠 적어라^^</span>
+        </div>
+        <div className="Login-box">
+          <div className="Login-box-text">로그인</div>
+          <div className="Login-box-form">
+            <input type="text" placeholder="이메일" />
+            <input type="password" placeholder="비밀번호" />
+            <div className="Login-box-save">
+              <input id="save_id" type="checkbox" className="Login-box-save-checkBox" />
+              <label htmlFor="save_id" />
+              <span>아이디 저장</span>
+            </div>
+          </div>
+          <div className="Login-box-button">
+            <div>
+              <span> 로그인 </span>
+            </div>
+            <span> 아이디 혹은 비밀번호를 잊으셨나요? </span>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
