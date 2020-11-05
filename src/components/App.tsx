d192c7241d02c11afbb5e84217dd9293db3a25db
Jominhui <rihito2021@naver.com>
2020-11-05 20:26:52 +0900

    [ADD] WritePage

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import LoginPage from "pages/LoginPage";
+import WritePage from "pages/WritePage";
 import { Route, Switch } from "react-router-dom";
 import MainPage from "../pages/MainPage";
 
@@ -9,6 +10,7 @@ const App = () => {
       <Switch>
         <Route exact path="/" component={MainPage} />
         <Route exact path="/login" component={LoginPage} />
+        <Route exact path="/Write" component={WritePage} />
       </Switch>
     </div>
   );
