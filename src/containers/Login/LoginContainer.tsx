641ee12a784b0d30f1f7e9dc233fd357b7105a98
정성훈 <52942411+jsh0128@users.noreply.github.com>
2020-12-07 13:49:42 +0900

    [ADD] accesToken set:LocalStroage & refrestToken setCooki

--- src/containers/Login/LoginContainer.tsx
+++ src/containers/Login/LoginContainer.tsx
@@ -4,6 +4,7 @@ import Login from "components/Login";
 import { LoginResponse } from "../../util/types/Response";
 import useStore from "../../util/lib/hooks/useStore";
 import { useHistory, withRouter } from "react-router-dom";
+import { useCookies } from "react-cookie";
 
 const LoginContainer = () => {
   const [check, setCheck] = useState<boolean>(false);
@@ -15,12 +16,16 @@ const LoginContainer = () => {
 
   const { tryLogin } = store.AuthStore;
 
+  const [cookie, setCookie, removeCookie] = useCookies(["refreshToken"]);
+
   const handleLogin = async () => {
     if (!id || !password) {
       console.log("아이디 또는 비밀번호를 입력해 주세요");
     } else {
       await tryLogin(id, password)
         .then((res: LoginResponse) => {
+          localStorage.setItem("accessToken", res.data.accessToken);
+          setCookie("refreshToken", res.data.refreshToken, { path: "/" });
           history.push("/");
         })
         .catch((err: Error) => {
