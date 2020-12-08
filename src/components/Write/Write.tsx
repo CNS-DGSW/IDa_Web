7f910347615f6c54c6793e0ad74b895d04ca295f
Jominhui <rihito2021@naver.com>
2020-12-08 22:36:47 +0900

    [FIX] Write

--- src/components/Write/Write.tsx
+++ src/components/Write/Write.tsx
@@ -1,11 +1,6 @@
 import React from "react";
-import WriteTitle from "components/common/WriteTitle";
-import WriteContent from "components/common/WriteContent";
-import WriteMember from "components/WriteMember";
-import WriteSchool from "components/WriteSchool";
-import WriteAdmission from "components/WriteAdmission";
-import WriteChoose from "components/WriteChoose";
-
+import WriteHeader from "../common/WriteHeader";
+import WriteContent from "../common/WriteContent";
 import "./Write.scss";
 
 interface WriteProps {}
@@ -15,26 +10,13 @@ const Write = ({}: WriteProps) => {
     <>
       <div className="write">
         <div className="write-box">
-          <WriteTitle />
-          <div className="write-formbox">
-            <WriteContent title="원서작성" children="원서 및 성적 입력" />
-            <div className="write-formbox-form">
-              <div className="write-formbox-form-menu">
-                <div>입학원서</div>
-                <div>성적알림표</div>
-                <div>자기소개서 및 학업계획서</div>
-              </div>
-              <div className="write-formbox-form-area">
-                <WriteMember />
-                <WriteSchool />
-                <WriteAdmission />
-                <WriteChoose />
-                <div className="write-formbox-form-btnarea">
-                  <div className="write-formbox-form-btnarea-btn">원서저장</div>
-                </div>
-              </div>
+          <WriteHeader />
+          <WriteContent title={"지원자의 정보를 입력해 주세요"}>
+            <div className="group">
+              <label>성명</label>
+              <input type="text" className="group-textInput" />
             </div>
-          </div>
+          </WriteContent>
         </div>
       </div>
     </>
