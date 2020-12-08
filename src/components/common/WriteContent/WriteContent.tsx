83280f7f690867bea0420bd432a128fd285c3fad
Jominhui <rihito2021@naver.com>
2020-12-08 22:35:49 +0900

    [ADD] WriteContent

--- src/components/common/WriteContent/WriteContent.tsx
+++ src/components/common/WriteContent/WriteContent.tsx
@@ -3,15 +3,22 @@ import "./WriteContent.scss";
 
 interface WriteContentProps {
   title: string;
-  children: string;
+  children: React.ReactNode;
 }
 
 const WriteContent = ({ title, children }: WriteContentProps) => {
   return (
     <>
-      <div className="title">
-        <span className="title-border">{title}</span>
-        <p>{children}</p>
+      <div className="writecontent">
+        <div className="writecontent-title">{title}</div>
+        <div className="writecontent-children">
+          <div className="writecontent-children-box">{children}</div>
+          <div className="writecontent-children-area">
+            <div className="writecontent-children-area-btn save">원서저장</div>
+            <div className="writecontent-children-area-btn preview">원서 미리보기</div>
+            <div className="writecontent-children-area-btn next">다음</div>
+          </div>
+        </div>
       </div>
     </>
   );
