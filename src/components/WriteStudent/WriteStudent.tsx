dd5e1bd6081cca25590085dcd6421ef6c994f6ed
Jominhui <rihito2021@naver.com>
2020-12-18 08:51:37 +0900

    ADD: WriteStudenet

---
+++ src/components/WriteStudent/WriteStudent.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import WriteContent from "../common/WriteContent";
+import "./WriteStudent.scss";
+
+interface WriteStudentProps {}
+
+const WriteStudent = ({}: WriteStudentProps) => {
+  return (
+    <>
+      <WriteContent title="지원자 정보를 입력해 주세요">
+        <div className="student">
+          <div className="student-text">
+            <label>성명</label>
+            <input type="text" className="student-text-textInput" />
+          </div>
+          <div className="student-select">
+            <div className="student-select-box">
+              <label>생년월일</label>
+              <div className="student-select-box-area">
+                <input type="text" className="student-select-box-area-Input7" />
+                <input type="text" className="student-select-box-area-Input5" />
+                <input type="text" className="student-select-box-area-Input5" />
+              </div>
+            </div>
+            <div className="student-select-box">
+              <label>성별</label>
+              <div className="student-select-box-area">
+                <input type="text" className="student-select-box-area-sexinput" />
+                <input type="text" className="student-select-box-area-sexinput" />
+              </div>
+            </div>
+          </div>
+          <div className="student-select">
+            <div className="student-select-box">
+              <label>집전화</label>
+              <div className="student-select-box-area">
+                <input type="text" className="student-select-box-area-Input5" />
+                <input type="text" className="student-select-box-area-Input6" />
+                <input type="text" className="student-select-box-area-Input6" />
+              </div>
+            </div>
+            <div className="student-select-box">
+              <label>휴대폰</label>
+              <div className="student-select-box-area">
+                <input type="text" className="student-select-box-area-Input5" />
+                <input type="text" className="student-select-box-area-Input6" />
+                <input type="text" className="student-select-box-area-Input6" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </WriteContent>
+    </>
+  );
+};
+
+export default WriteStudent;
