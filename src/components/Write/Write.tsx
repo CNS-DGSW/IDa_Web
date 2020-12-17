b0c5155ee385e596c9e6365882552ddeabff307d
Jominhui <rihito2021@naver.com>
2020-12-18 08:53:09 +0900

    FIX: style

--- src/components/Write/Write.tsx
+++ src/components/Write/Write.tsx
@@ -1,22 +1,72 @@
 import React from "react";
-import WriteHeader from "../common/WriteHeader";
-import WriteContent from "../common/WriteContent";
+import WriteStudent from "../WriteStudent";
+import StepProgressBar from "react-step-progress";
+import "react-step-progress/dist/index.css";
 import "./Write.scss";
 
 interface WriteProps {}
 
 const Write = ({}: WriteProps) => {
+  const writeStudent = <WriteStudent></WriteStudent>;
+  const writeContent = <WriteStudent></WriteStudent>;
+
+  function onFormSubmit() {
+    // handle the submit logic here
+    // This function will be executed at the last step
+    // when the submit button (next button in the previous steps) is pressed
+  }
   return (
     <>
       <div className="write">
         <div className="write-box">
-          <WriteHeader />
-          <WriteContent title={"지원자의 정보를 입력해 주세요"}>
-            <div className="group">
-              <label>성명</label>
-              <input type="text" className="group-textInput" />
-            </div>
-          </WriteContent>
+          <StepProgressBar
+            startingStep={0}
+            onSubmit={onFormSubmit}
+            steps={[
+              {
+                label: "1단계",
+                subtitle: "",
+                name: "step 1",
+                content: writeStudent,
+              },
+              {
+                label: "2단계",
+                subtitle: "",
+                name: "step 2",
+                content: "",
+              },
+              {
+                label: "3단계",
+                subtitle: "",
+                name: "step 3",
+                content: "",
+              },
+              {
+                label: "4단계",
+                subtitle: "",
+                name: "step 4",
+                content: "",
+              },
+              {
+                label: "5단계",
+                subtitle: "",
+                name: "step 5",
+                content: "",
+              },
+              {
+                label: "6단계",
+                subtitle: "",
+                name: "step 6",
+                content: "",
+              },
+              {
+                label: "7단계",
+                subtitle: "",
+                name: "step 7",
+                content: "",
+              },
+            ]}
+          />
         </div>
       </div>
     </>
