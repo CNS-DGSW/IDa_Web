e5eb682fe3020da65d33b68995cee6f2e012da39
Jominhui <rihito2021@naver.com>
2020-12-18 11:11:18 +0900

    ADD: introduce Page

---
+++ src/components/WriteIntroduction/WriteIntroduction.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import WriteContent from "../common/WriteContent";
+import "./WriteIntroduction.scss";
+
+interface WriteIntroductionProps {}
+
+const WriteIntroduction = ({}: WriteIntroductionProps) => {
+  return (
+    <>
+      <WriteContent title="자기소개서 및 학업계획서를 작성(제출)해주세요" idx="7">
+        <div></div>
+      </WriteContent>{" "}
+    </>
+  );
+};
+
+export default WriteIntroduction;
