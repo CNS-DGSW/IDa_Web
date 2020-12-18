39e947d5910842a4cd21b63d7bf3c744a5163731
Jominhui <rihito2021@naver.com>
2020-12-18 11:10:15 +0900

    ADD: Admossion Page

--- src/components/Write/Write.tsx
+++ src/components/Write/Write.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import WriteStudent from "../WriteStudent";
 import WriteParent from "../WriteParents";
 import WritePhoto from "../WritePhoto";
+import WriteSchool from "../WriteSchool";
+import WriteAdmission from "../WriteAdmission";
+import WriteGrades from "../WriteGrades";
+import WriteIntroduction from "../WriteIntroduction";
 import StepProgressBar from "react-step-progress";
 import "react-step-progress/dist/index.css";
 import "./Write.scss";
@@ -12,6 +16,10 @@ const Write = ({}: WriteProps) => {
   const writeStudent = <WriteStudent></WriteStudent>;
   const writeParent = <WriteParent></WriteParent>;
   const writePhoto = <WritePhoto></WritePhoto>;
+  const writeSchool = <WriteSchool></WriteSchool>;
+  const writeAdmission = <WriteAdmission></WriteAdmission>;
+  const writeGrades = <WriteGrades></WriteGrades>;
+  const writeIntroduction = <WriteIntroduction></WriteIntroduction>;
 
   function onFormSubmit() {
     // handle the submit logic here
@@ -48,25 +56,25 @@ const Write = ({}: WriteProps) => {
                 label: "4단계",
                 subtitle: "",
                 name: "step 4",
-                content: "",
+                content: writeSchool,
               },
               {
                 label: "5단계",
                 subtitle: "",
                 name: "step 5",
-                content: "",
+                content: writeAdmission,
               },
               {
                 label: "6단계",
                 subtitle: "",
                 name: "step 6",
-                content: "",
+                content: writeGrades,
               },
               {
                 label: "7단계",
                 subtitle: "",
                 name: "step 7",
-                content: "",
+                content: writeIntroduction,
               },
             ]}
           />
--- src/components/WriteAdmission/WriteAdmission.tsx
+++ src/components/WriteAdmission/WriteAdmission.tsx
@@ -1,42 +1,15 @@
 import React from "react";
 import "./WriteAdmission.scss";
-import WriteContent from "components/common/WriteContent";
+import WriteContent from "../common/WriteContent";
 
 interface WriteAdmissionProps {}
 
 const WriteAdmission = ({}: WriteAdmissionProps) => {
   return (
     <>
-      <div className="Admission">
-        <WriteContent title="전형구분" children="전형 및 그에 따른 해당사항 선택" />
-        <div className="Admission-box">
-          <WriteContent title="입학전형" children=" " />
-          <div className="Admission-box-content">
-            <div className="Admission-box-content-inputgroup">
-              <div className="Admission-box-content-inputgroup-check">일반전형</div>
-              <div className="Admission-box-content-inputgroup-check">특별전형</div>
-              <div className="Admission-box-content-inputgroup-check">특례입학</div>
-            </div>
-            <div className="Admission-box-content-info"></div>
-          </div>
-        </div>
-
-        <div className="Admission-box">
-          <WriteContent title="특별전형" children=" " />
-          <div className="Admission-box-content">
-            <div className="Admission-box-content-inputgroup">
-              <div className="Admission-box-content-inputgroup-check">마이스터인재</div>
-              <div className="Admission-box-content-inputgroup-check">기회균등전형</div>
-              <div className="Admission-box-content-inputgroup-check">사회다양성전형</div>
-              <div className="Admission-box-content-inputgroup-check">지역우선전형</div>
-            </div>
-            <div>
-              <div className="Admission-box-content-select">선택해주세요.</div>
-              <div className="Admission-box-content-info"></div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <WriteContent title="전형 및 그에 따른 해당사항을 선택해주세요" idx="5">
+        <div></div>
+      </WriteContent>
     </>
   );
 };
