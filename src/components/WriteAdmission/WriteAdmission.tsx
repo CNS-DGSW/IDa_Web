7008dee1f5a14d7907b7315b962faf6b0d046d93
Jominhui <rihito2021@naver.com>
2021-01-11 06:36:53 +0900

    [ADD] admission

--- src/components/WriteAdmission/WriteAdmission.tsx
+++ src/components/WriteAdmission/WriteAdmission.tsx
@@ -2,13 +2,55 @@ import React from "react";
 import "./WriteAdmission.scss";
 import WriteContent from "../common/WriteContent";
 
-interface WriteAdmissionProps {}
+interface WriteAdmissionProps {
+  page: number;
+  nextPage: () => void;
+  prevPage: () => void;
+}
 
-const WriteAdmission = ({}: WriteAdmissionProps) => {
+const WriteAdmission = ({ page, nextPage, prevPage }: WriteAdmissionProps) => {
   return (
     <>
-      <WriteContent title="전형 및 그에 따른 해당사항을 선택해주세요" idx="5">
-        <div></div>
+      <WriteContent
+        title="전형 및 그에 따른 해당사항을 선택해주세요"
+        page={page}
+        nextPage={nextPage}
+        prevPage={prevPage}
+      >
+        <div className="school">
+          <div className="school-area">
+            <label className="school-area-label">입학전형 선택</label>
+            <div className="school-area-select">
+              <label className="school-area-select-box">
+                일반전형
+                <input
+                  type="radio"
+                  name="school"
+                  value="nomal"
+                  className="school-area-select-box-selectinput"
+                />
+              </label>
+              <label className="school-area-select-box">
+                특별전형
+                <input
+                  type="radio"
+                  name="school"
+                  value="special"
+                  className="school-area-select-box-selectinput"
+                />
+              </label>
+              <label className="school-area-select-box">
+                특례입학
+                <input
+                  type="radio"
+                  name="school"
+                  value="specialcase"
+                  className="school-area-select-box-selectinput"
+                />
+              </label>
+            </div>
+          </div>
+        </div>
       </WriteContent>
     </>
   );
