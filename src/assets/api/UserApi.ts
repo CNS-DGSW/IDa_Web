9351efbf7115d957b957d9b246fa8bf03e3ca7e4
Jominhui <rihito2021@naver.com>
2021-01-23 23:56:12 +0900

    Update:  store & api

--- src/assets/api/UserApi.ts
+++ src/assets/api/UserApi.ts
@@ -1,6 +1,8 @@
 import Api from "lib/customAxios";
 import Apply from "util/enums/Apply";
 import ApplyDetail from "util/enums/ApplyDetail";
+import Grade from "util/enums/Grade";
+import Relation from "util/enums/Relation";
 import Sex from "util/enums/Sex";
 
 class UserApi {
@@ -45,8 +47,82 @@ class UserApi {
         verteransNumber,
       };
 
-      const { data } = await Api.patch("/user/editInfo", body);
+      const { data } = await Api.patch("/user/editApplyType", body);
+      return data;
+    } catch (error) {
+      throw new Error(`${error}`);
+    }
+  }
 
+  async EditParentInfo(
+    address: string,
+    parentName: string,
+    parentRelation: Relation,
+    parentTel: string,
+    postCode: string
+  ) {
+    try {
+      const body = {
+        address,
+        parentName,
+        parentRelation,
+        parentTel,
+        postCode,
+      };
+
+      const { data } = await Api.patch("/user/editParentInfo", body);
+
+      return data;
+    } catch (error) {
+      throw new Error(`${error}`);
+    }
+  }
+
+  async getParentInfo() {
+    try {
+      const { data } = await Api.get("/user/getParentInfo");
+
+      return data;
+    } catch (error) {
+      throw new Error(`${error}`);
+    }
+  }
+
+  async editSchoolInfo(
+    cityLocation: string,
+    cityName: string,
+    gradeType: Grade,
+    graduatedDate: string,
+    schoolCode: string,
+    schoolName: string,
+    schoolTel: string,
+    teacherName: string,
+    teacherTel: string
+  ) {
+    try {
+      const body = {
+        cityLocation,
+        cityName,
+        gradeType,
+        graduatedDate,
+        schoolCode,
+        schoolName,
+        schoolTel,
+        teacherName,
+        teacherTel,
+      };
+
+      const { data } = await Api.patch("/user/editSchoolInfo", body);
+
+      return data;
+    } catch (error) {
+      throw new Error(`${error}`);
+    }
+  }
+
+  async getSchoolInfo() {
+    try {
+      const { data } = await Api.get("/user/getSchoolInfo");
       return data;
     } catch (error) {
       throw new Error(`${error}`);
--- src/stores/Write/WriteStore.ts
+++ src/stores/Write/WriteStore.ts
@@ -1,8 +1,16 @@
 import { action, observable } from "mobx";
 import { autobind } from "core-decorators";
 import AuthApi from "assets/api/AuthApi";
-import { ApplyTypeResponse, Response, UserInfoResponse } from "util/types/Response";
+import {
+  ApplyTypeResponse,
+  ParentInfoResponse,
+  Response,
+  UserInfoResponse,
+  SchoolInfoResponse,
+} from "util/types/Response";
 import Sex from "util/enums/Sex";
+import Relation from "util/enums/Relation";
+import Grade from "util/enums/Grade";
 import UserApi from "assets/api/UserApi";
 import Apply from "util/enums/Apply";
 import ApplyDetail from "util/enums/ApplyDetail";
@@ -10,10 +18,16 @@ import ApplyDetail from "util/enums/ApplyDetail";
 @autobind
 class WriteStore {
   @observable page: number = 0;
+  @observable gradeType: Grade | null = null;
 
   @action
   pageHandle = (page: number) => {
     this.page = page;
+  };
+
+  @action
+  handleGrade = (gradeType: Grade | null) => {
+    this.gradeType = gradeType;
   };
 
   @action
@@ -67,6 +81,21 @@ class WriteStore {
   };
 
   @action
+  getParentInfo = async (): Promise<ParentInfoResponse> => {
+    try {
+      const response: ParentInfoResponse = await UserApi.getParentInfo();
+
+      return new Promise((resolve: (response: ParentInfoResponse) => void, reject) => {
+        resolve(response);
+      });
+    } catch (error) {
+      return new Promise((resolve, reject: (error: Error) => void) => {
+        reject(error);
+      });
+    }
+  };
+
+  @action
   editApplyType = async (
     applyType: Apply,
     applyDetailType: ApplyDetail,
@@ -79,6 +108,83 @@ class WriteStore {
         applyDetailType,
         verteransCity,
         verteransNumber
+      );
+
+      return new Promise((resolve: (response: Response) => void, reject) => {
+        resolve(response);
+      });
+    } catch (error) {
+      return new Promise((resolve, reject: (error: Error) => void) => {
+        reject(error);
+      });
+    }
+  };
+
+  @action
+  editParentInfo = async (
+    address: string,
+    parentName: string,
+    parentRelation: Relation,
+    parentTel: string,
+    postcode: string
+  ): Promise<Response> => {
+    try {
+      const response: Response = await UserApi.EditParentInfo(
+        address,
+        parentName,
+        parentRelation,
+        parentTel,
+        postcode
+      );
+
+      return new Promise((resolve: (response: Response) => void, reject) => {
+        resolve(response);
+      });
+    } catch (error) {
+      return new Promise((resolve, reject: (error: Error) => void) => {
+        reject(error);
+      });
+    }
+  };
+
+  @action
+  getSchoolInfo = async (): Promise<SchoolInfoResponse> => {
+    try {
+      const response: SchoolInfoResponse = await UserApi.getSchoolInfo();
+
+      return new Promise((resolve: (response: SchoolInfoResponse) => void, reject) => {
+        resolve(response);
+      });
+    } catch (error) {
+      return new Promise((resolve, reject: (error: Error) => void) => {
+        reject(error);
+      });
+    }
+  };
+
+  @action
+  editSchoolInfo = async (
+    cityLocation: string,
+    cityName: string,
+    gradeType: Grade,
+    graduatedDate: string,
+    schoolCode: string,
+    schoolName: string,
+    schoolTel: string,
+    teacherName: string,
+    teacherTel: string
+  ): Promise<Response> => {
+    try {
+      const response: Response = await UserApi.editSchoolInfo(
+        cityLocation,
+        cityName,
+        gradeType,
+        graduatedDate,
+        schoolCode,
+        schoolName,
+        schoolTel,
+        teacherName,
+        teacherTel
       );
 
       return new Promise((resolve: (response: Response) => void, reject) => {
