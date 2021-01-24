4aea59b896558df73d641b86e62bddf33a2c5d44
Sh031224 <1cktmdgh2@gmail.com>
2021-01-25 08:01:58 +0900

    Update: Header

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./Header.scss";
 import { ReactComponent as Profile } from "assets/images/profile.svg";
 import ProfileModalBox from "components/Profile/ProfileModalBox";
-import { useHistory } from "react-router-dom";
+import { NavLink, useHistory } from "react-router-dom";
 import { ReactComponent as Logo1 } from "assets/images/logo-1.svg";
 import { ReactComponent as Logo2 } from "assets/images/logo-2.svg";
 
@@ -31,11 +31,49 @@ const Header = ({
     <header className={theme ? "header header-theme" : "header"} id="header">
       <div className="header-container">
         <>
-          {theme ? (
-            <Logo2 className="pointer" onClick={() => history.push("/")} />
-          ) : (
-            <Logo1 className="pointer" onClick={() => history.push("/")} />
-          )}
+          <div className="header-container-link">
+            {theme ? (
+              <Logo2 className="pointer" onClick={() => history.push("/")} />
+            ) : (
+              <Logo1 className="pointer" onClick={() => history.push("/")} />
+            )}
+            <NavLink
+              to="/"
+              exact
+              className="header-container-link-item"
+              activeClassName="header-container-link-item-active"
+            >
+              <span>홈</span>
+            </NavLink>
+            <NavLink
+              to="/write"
+              className="header-container-link-item"
+              activeClassName="header-container-link-item-active"
+            >
+              <span>원서접수</span>
+            </NavLink>
+            <NavLink
+              to="/notice"
+              className="header-container-link-item"
+              activeClassName="header-container-link-item-active"
+            >
+              <span>공지사항</span>
+            </NavLink>
+            <NavLink
+              to="/qna"
+              className="header-container-link-item"
+              activeClassName="header-container-link-item-active"
+            >
+              <span>Q&A</span>
+            </NavLink>
+            <NavLink
+              to="/faq"
+              className="header-container-link-item"
+              activeClassName="header-container-link-item-active"
+            >
+              <span>FAQ</span>
+            </NavLink>
+          </div>
           {login ? (
             <>
               <Profile
