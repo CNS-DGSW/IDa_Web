0baf022f1f76732f422739d94ea250d57c3a975a
Sh031224 <1cktmdgh2@gmail.com>
2021-01-25 07:47:46 +0900

    Add: 전형 다운로드

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,11 +1,13 @@
 import AuthStore from "./Auth";
 import WriteStore from "./Write";
 import BoardStore from "./Board";
+import FileStore from "./File";
 
 const stores = {
   AuthStore: new AuthStore(),
   WriteStore: new WriteStore(),
   BoardStore: new BoardStore(),
+  FileStore: new FileStore(),
 };
 
 export default stores;
--- src/util/types/StoreType.ts
+++ src/util/types/StoreType.ts
@@ -1,12 +1,14 @@
 import AuthStore from "../../stores/Auth";
 import WriteStore from "../../stores/Write";
 import BoardStore from "../../stores/Board";
+import FileStore from "stores/File";
 
 type StoreType = {
   store: {
     AuthStore: AuthStore;
     WriteStore: WriteStore;
     BoardStore: BoardStore;
+    FileStore: FileStore;
   };
 };
 
