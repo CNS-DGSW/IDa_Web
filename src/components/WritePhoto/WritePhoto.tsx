284d7e13b1f3abab98a89c142b1646ffac9e8f2d
Jominhui <rihito2021@naver.com>
2021-01-24 20:58:49 +0900

    ADD: photo page

--- src/components/WritePhoto/WritePhoto.tsx
+++ src/components/WritePhoto/WritePhoto.tsx
@@ -2,13 +2,30 @@ import React from "react";
 import "./WritePhoto.scss";
 import WriteContent from "../common/WriteContent";
 
-interface WritePhotoProps {}
+interface WritePhotoProps {
+  preview: string | ArrayBuffer | null;
+  handleImageChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
 
-const WritePhoto = ({}: WritePhotoProps) => {
+const WritePhoto = ({ handleImageChange, preview }: WritePhotoProps) => {
   return (
     <>
       <WriteContent title="지원자의 사진을 등록해 주세요" onSave={() => console.log(1)}>
-        <div></div>
+        <div className="photo">
+          {preview ? (
+            <img src={preview.toString()} className="photo-preview" />
+          ) : (
+            <div className="photo-preview">미리보기</div>
+          )}
+          <label htmlFor="preview">사진 선택</label>
+          <input
+            type="file"
+            id="preview"
+            onChange={handleImageChange}
+            accept=".jpg, .jpeg, .gif"
+          />
+          <div>* 2MB 이내의 jpg, jpeg, gif 사진을 등록해주세요.</div>
+        </div>
       </WriteContent>
     </>
   );
--- src/containers/WritePhoto/WritePhotoContainer.tsx
+++ src/containers/WritePhoto/WritePhotoContainer.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { observer } from "mobx-react";
 import WritePhoto from "../../components/WritePhoto";
 import useStore from "lib/hooks/useStore";
@@ -6,9 +6,26 @@ import useStore from "lib/hooks/useStore";
 const WritePhotoContainer = ({}) => {
   const { store } = useStore();
 
+  const [preview, setPreview] = useState<string | ArrayBuffer | null>("");
+  const [image, setImage] = useState<File | Blob | null>();
+
+  function handleImageChange(e: React.ChangeEvent<HTMLInputElement>) {
+    let reader = new FileReader();
+    if (e.target.files && e.target.files.length) {
+      let file = e.target.files[0];
+      reader.onloadend = () => {
+        setImage(file);
+        setPreview(reader.result);
+      };
+      reader.readAsDataURL(file);
+    } else {
+      setPreview("");
+    }
+  }
+
   return (
     <>
-      <WritePhoto></WritePhoto>
+      <WritePhoto preview={preview} handleImageChange={handleImageChange}></WritePhoto>
     </>
   );
 };
