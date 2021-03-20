3b03c5bb6564d98c55c62ff4399a200c6859be93
Sh031224 <1cktmdgh2@gmail.com>
2021-03-20 09:29:58 +0900

    Fix: Types

--- src/util/types/Response.ts
+++ src/util/types/Response.ts
@@ -10,8 +10,9 @@ import ScoreGrade from "./ScoreGrade";
 import { Receipt } from "./ReceiptType";
 import { SchoolCity } from "./SchoolCity";
 import { CityRatio, DateRatio, SchoolRatio } from "./UserRatio";
-import { List, ListPassed, Rate } from "./User";
+import { List, ListPassed, Rate } from "./UserList";
 import { Report } from "./ReportInfo";
+import InterViewCategory from "util/enums/InterViewCategory";
 
 export type Response = {
   status: number;
@@ -236,4 +237,54 @@ export interface UserRate extends Response {
 
 export interface ReportInfo extends Response {
   data: Report[];
+}
+
+export interface SecondScoreResponse extends Response {
+  data: [
+    {
+      absenceScore: number;
+      additionalScore: number;
+      applyDetailType: Apply | null;
+      applyType: Apply | null;
+      cityName: string;
+      codingTestScore: number;
+      cooperationScore: number;
+      examCode: number;
+      finalApplyDetailType: Apply | null;
+      finalApplyType: Apply | null;
+      gradeScore: number;
+      gradeType: Grade | null;
+      interviewScore: number;
+      jobAptitudeScore: number;
+      swAbilityScore: number;
+      totalInterviewScore: number;
+      totalScore: number;
+      userIdx: number;
+      userName: string;
+      volunteerScore: number;
+      isPassed: boolean | null;
+    }
+  ];
+}
+
+export interface TeamResponse extends Response {
+  data: number[];
+}
+
+export interface InterViewScoreResponse extends Response {
+  data: [
+    {
+      calcScore: number;
+      evaluationFactor1: number;
+      evaluationFactor2: number;
+      evaluationFactor3: number;
+      evaluationFactor4: number;
+      evaluationFactor5: number;
+      evaluationFactor6: number | null;
+      evaluationFactor7: number | null;
+      examCode: number;
+      interviewCategory: InterViewCategory;
+      userName: string;
+    }
+  ];
 }
