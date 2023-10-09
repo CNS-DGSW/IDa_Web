661859b286d266d2aef0ad22802bdf907290b3b2
bae0203 <gmbae06@dgsw.hs.kr>
2023-10-09 14:22:48 +0900

    Add : useIsApplyPeriod

---
+++ src/lib/hooks/useIsApplyPeriod.ts
@@ -0,0 +1,20 @@
+import { useLayoutEffect, useState } from "react";
+
+const currentDate = new Date();
+
+const useIsApplyPeriod = () => {
+  const [isTest, setIsTest] = useState<boolean>(true);
+  useLayoutEffect(() => {
+    const targetDate = new Date(currentDate.getFullYear(), 9, 10, 9, 0, 0);
+    if (currentDate >= targetDate) {
+      console.log("10월 10일 9시 이후입니다.");
+      setIsTest(true);
+    } else {
+      console.log("10월 10일 9시 이wjsjsjs입니다.");
+      setIsTest(false);
+    }
+  }, []);
+  return isTest;
+};
+
+export default useIsApplyPeriod;
