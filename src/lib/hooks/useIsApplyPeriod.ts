fcbc98afca860b6977d4c26b5f2a9b2b38bb5df9
bae0203 <gmbae06@dgsw.hs.kr>
2023-10-11 11:47:03 +0900

    fix : Time Limit hooks

--- src/lib/hooks/useIsApplyPeriod.ts
+++ src/lib/hooks/useIsApplyPeriod.ts
@@ -5,13 +5,21 @@ const currentDate = new Date();
 const useIsApplyPeriod = () => {
   const [isTest, setIsTest] = useState<boolean>(true);
   useEffect(() => {
-    const targetDate = new Date(currentDate.getFullYear(), 9, 10, 9, 0, 0);
-    if (currentDate >= targetDate) {
+    const targetDatebefore = new Date(
+      currentDate.getFullYear(),
+      9,
+      15,
+      17,
+      0,
+      0
+    );
+    const targetDateafter = new Date(currentDate.getFullYear(), 9, 16, 9, 0, 0);
+    if (currentDate >= targetDatebefore && currentDate <= targetDateafter) {
       //   console.log("10월 10일 9시 이후입니다.");
-      setIsTest(true);
+      setIsTest(false);
     } else {
       //   console.log("10월 10일 9시 이wjsjsjs입니다.");
-      setIsTest(false);
+      setIsTest(true);
     }
   }, []);
   return isTest;
--- src/lib/hooks/useTimeLimit.ts
+++ src/lib/hooks/useTimeLimit.ts
@@ -5,23 +5,28 @@ const currentDate = new Date();
 const useTimeLimit = () => {
   // 원서접수 접근 시간 관리
 
-  // 10/11(수) 9시 ~ 10/13(금) 17시 (시범운영)
+  // 10/11(수) 9시 ~ 10/15(일) 17시 (시범운영)
   // 10/16(월) 9시 ~ 10/19(목) 17시 (정식운영)
   const WriteFirstPeriodStart = new Date(2023, 9, 11, 9, 0, 0);
-  const WriteFirstPeriodEnd = new Date(2023, 9, 13, 17, 0, 0);
+  const WriteFirstPeriodEnd = new Date(2023, 9, 15, 17, 0, 0);
   const WriteSecondPeriodStart = new Date(2023, 9, 16, 9, 0, 0);
   const WriteSecondPeriodEnd = new Date(2023, 9, 19, 17, 0, 0);
 
   const [canAccessWrite, setCanAccessWrite] = useState<boolean>(true);
   const WriteLimitControl = () => {
-    /*
-        if (currentDate >= WriteFirstPeriodStart && currentDate <= WriteFirstPeriodEnd){
-            setCanAccessWrite(true)
-        } else if (currentDate >= WriteSecondPeriodStart && currentDate <= WriteSecondPeriodEnd){
-            setCanAccessWrite(true)
-        } else {
-            setCanAccessWrite(false)
-        } */
+    if (
+      currentDate >= WriteFirstPeriodStart &&
+      currentDate <= WriteFirstPeriodEnd
+    ) {
+      setCanAccessWrite(true);
+    } else if (
+      currentDate >= WriteSecondPeriodStart &&
+      currentDate <= WriteSecondPeriodEnd
+    ) {
+      setCanAccessWrite(true);
+    } else {
+      setCanAccessWrite(false);
+    }
     return canAccessWrite;
   };
 
@@ -35,11 +40,11 @@ const useTimeLimit = () => {
 
   const [canAccessSignup, setCanAccessSignup] = useState<boolean>(true);
   const SignupLimitControl = () => {
-    /* if(currentDate >= SignupPeriodStart && currentDate <= SignupPeriodEnd){
-            setCanAccessSignup(true)
-        } else {
-            setCanAccessSignup(false)
-        } */
+    if (currentDate >= SignupPeriodStart && currentDate <= SignupPeriodEnd) {
+      setCanAccessSignup(true);
+    } else {
+      setCanAccessSignup(false);
+    }
     return canAccessSignup;
   };
 
