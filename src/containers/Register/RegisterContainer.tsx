3061c2f61ccc9c7f1b1421f54431c3d70bd12698
bae0203 <gmbae06@dgsw.hs.kr>
2023-10-12 15:37:18 +0900

    fix : toast Value

--- src/containers/Register/RegisterContainer.tsx
+++ src/containers/Register/RegisterContainer.tsx
@@ -219,7 +219,10 @@ const RegisterContainer = () => {
             // toast.warning(`${err.response?.data.message}`);
             toast.warning("이메일 인증이 진행되지 않았습니다.");
           } else if (err.response?.status === 400) {
-            toast.warning("이미 사용중인 전화번호입니다.");
+            toast.warning(
+              "전화번호 혹은 이메일 양식을 다시 한 번 확인해주세요."
+            );
+            console.log(err.response);
           } else if (err.response?.status === 410) {
             toast.warning("이미 사용중인 실명인증입니다.");
           } else {
