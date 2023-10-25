7dc118338aa6ca76448cd4d8ba866031e02f1782
chldkduds2 <101560578+chldkduds2@users.noreply.github.com>
2023-10-25 11:07:33 +0900

    FIX : 원서접수일정을 2차전형일정으로 수정

--- src/models/submitTime.ts
+++ src/models/submitTime.ts
@@ -8,3 +8,9 @@ export const submitEndTime = moment("2023-10-19 17:00:00");
 export const firstTime = moment("2023-10-25 10:00:00");
 // 최종 합격 발표
 export const finalTime = moment("2023-11-01 10:00:00");
+
+//2차 전형 시작 날짜
+export const secondaryStartTime = moment("2023-10-27 10:00:00");
+
+//2차 전형 마감 날짜
+export const secondaryEndTime = moment("2023-10-27 16:40:00");
